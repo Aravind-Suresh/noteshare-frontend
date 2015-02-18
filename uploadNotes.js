4c4fd7904897ff4b890753f51753b0e71bfca3ad
aravind <arvindsuresh2009@gmail.com>
2015-02-18 22:44:15 +0530

    modal for upload notes done

---
+++ uploadNotes.js
@@ -0,0 +1,9 @@
+var uploadNotes = function($scope, $rootScope, $http) {
+	$scope.uploadNotes = function() {
+		console.log('Notes uploaded');
+	}
+
+	$scope.loadImage = function() {
+		console.log('Image loaded');
+	}
+}
