289cea2dabfab29361700690e215d81b00e9e378
aravind <arvindsuresh2009@gmail.com>
2015-02-19 13:17:07 +0530

    small nav-link part done

--- myProfile.js
+++ myProfile.js
@@ -2,7 +2,12 @@ function myProfile($scope) {
 	// TODO get info from backend and populate user class object
 	console.log('started');
 	$scope.user = {
-		"userName":"Aravind Shankar"
+		"userName":"Aravind Shankar",
+		"uploadsCount":"50",
+		"downloadsCount":"60",
+		"creditsCount":"10",
+		"followersCount":"40",
+		"followingCount":"0"
 	};
 	$scope.uploadNotes = function() {
 		console.log('Notes uploaded');
