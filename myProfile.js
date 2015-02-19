427cca1ea544e5b9ad28f209b51dfb3caf694db8
aravind <arvindsuresh2009@gmail.com>
2015-02-19 23:24:27 +0530

    working on notification

--- myProfile.js
+++ myProfile.js
@@ -1,5 +1,6 @@
 function myProfile($scope) {
 	// TODO get info from backend and populate user class object
+	$scope.notificationCount = 1;
 	console.log('started');
 	$scope.user = {
 		"userName":"Aravind Shankar",
@@ -9,6 +10,7 @@ function myProfile($scope) {
 		"followersCount":"40",
 		"followingCount":"0"
 	};
+	$scope.user.notifications=[];
 	$scope.uploadNotes = function() {
 		console.log('Notes uploaded');
 	}
@@ -23,5 +25,15 @@ function myProfile($scope) {
 	}
 	$scope.logout = function() {
 		console.log('logged out');
+	}
+	$scope.updateNotificationCounter = function() {
+		console.log('called update function');
+		$('#notificationCount').css({opacity:0});
+		$scope.notificationCount = $scope.notificationCount + 1;
+		$('#notificationCount').css({top: '-30px'});
+		$('#notificationCount').animate({top: '12px', opacity: 1});
+	}
+	$scope.updateNotification = function() {
+		$scope.user.notifications.push({"textHeading":"New notification","textDescription":"Description about the notification"});
 	}
 }
